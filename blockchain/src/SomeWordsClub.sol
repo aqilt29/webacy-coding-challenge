4068dd38eca34f7c1eb31a5ab71bc4d3f66081bf
Aqil Thanawala <aqil.thanawala@gmail.com>
2022-06-27 19:46:48 +0100

    tests show console in verbose mode

--- blockchain/src/SomeWordsClub.sol
+++ blockchain/src/SomeWordsClub.sol
@@ -1,12 +1,40 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "forge-std/console2.sol";
 import "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
+import "openzeppelin-contracts/contracts/utils/Strings.sol";
 
 contract SomeWordsClub is ERC1155 {
+  using Strings for uint256;
 
-  constructor() ERC1155("") {
-    
+  uint256 public constant CLOUD = 0;
+  uint256 public constant FRIEND = 1;
+  uint256 public constant WORK = 2;
+  uint256 public constant PLEASURE = 3;
+  uint256 public constant SUCCESS = 4;
+
+  uint256 public constant MAX_SUPPLY = 2500;
+  string public name = "Some Words Club";
+  string public baseURI;
+
+  mapping(uint256 => bool) public isValidTokenId;
+
+
+  constructor(string memory _tokenUri) ERC1155(_tokenUri) {
+    console2.log(_tokenUri, " <== baseUri");
+    baseURI = _tokenUri;
+    isValidTokenId[0] = true;
+    isValidTokenId[1] = true;
+    isValidTokenId[2] = true;
+    isValidTokenId[3] = true;
+    isValidTokenId[4] = true;
   }
+
+  function uri(uint256 tokenId) public view virtual override returns (string memory) {
+    require(isValidTokenId[tokenId] == true, "ERROR, out of bounds tokenId");
+    return string(abi.encodePacked(baseURI, tokenId.toString(), ".json"));
+  }
+
 
 }
--- blockchain/test/Contract.t.sol
+++ blockchain/test/Contract.t.sol
@@ -2,11 +2,27 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "forge-std/console2.sol";
+import "src/SomeWordsClub.sol";
 
 contract ContractTest is Test {
-    function setUp() public {}
+  SomeWordsClub public someWordsClub;
+  string public defaultUri = "ipfs://QmcGda9o8iy5tB197fG8tVF4HzWPrbxHY4psu17gog2jX4/";
 
-    function testExample() public {
-        assertTrue(true);
-    }
+  function setUp() public {
+    someWordsClub = new SomeWordsClub(defaultUri);
+
+  }
+
+  function testUri() public {
+    string memory foundUri = someWordsClub.uri(0);
+    console2.log(foundUri, "<== foundUri");
+
+    assertTrue(keccak256(bytes(foundUri)) == keccak256(bytes("ipfs://QmcGda9o8iy5tB197fG8tVF4HzWPrbxHY4psu17gog2jX4/0.json")));
+  }
+
+  function testFailUriOutOfBounds() public view {
+    someWordsClub.uri(10);
+  }
+
 }
