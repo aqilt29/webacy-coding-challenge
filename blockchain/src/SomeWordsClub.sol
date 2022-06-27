28321de529594ceaa7c4bb6dc8280f3d02c37903
Aqil Thanawala <aqil.thanawala@gmail.com>
2022-06-27 16:14:45 +0100

    include gif and change nft contract name

--- blockchain/src/SomeWordsClub.sol
+++ blockchain/src/SomeWordsClub.sol
@@ -5,7 +5,7 @@ import "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
 
 contract SomeWordsClub is ERC1155 {
 
-  constructor() ERC1155("ipfs://QmcGda9o8iy5tB197fG8tVF4HzWPrbxHY4psu17gog2jX4") {
+  constructor() ERC1155("") {
     
   }
 
