070bfdef751b20876769440ab258bc5bfeb9d285
Aqil Thanawala <aqil.thanawala@gmail.com>
2022-06-28 23:12:38 +0100

    add minting and testing for minting

--- blockchain/src/SomeWordsClub.sol
+++ blockchain/src/SomeWordsClub.sol
@@ -3,10 +3,15 @@ pragma solidity ^0.8.13;
 
 import "forge-std/console2.sol";
 import "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
+import "openzeppelin-contracts/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "openzeppelin-contracts/contracts/utils/Strings.sol";
+import "openzeppelin-contracts/contracts/utils/Address.sol";
+import "openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract SomeWordsClub is ERC1155 {
+contract SomeWordsClub is ERC1155, ERC1155Supply, ReentrancyGuard, Ownable {
   using Strings for uint256;
+  using Address for address;
 
   uint256 public constant CLOUD = 0;
   uint256 public constant FRIEND = 1;
@@ -15,6 +20,10 @@ contract SomeWordsClub is ERC1155 {
   uint256 public constant SUCCESS = 4;
 
   uint256 public constant MAX_SUPPLY = 2500;
+
+  // 0.0025 ETH
+  uint256 public constant MINT_PRICE = 2500000000000000;
+  uint256 public MINT_LIMIT = 5;
   string public name = "Some Words Club";
   string public baseURI;
 
@@ -22,7 +31,6 @@ contract SomeWordsClub is ERC1155 {
 
 
   constructor(string memory _tokenUri) ERC1155(_tokenUri) {
-    console2.log(_tokenUri, " <== baseUri");
     baseURI = _tokenUri;
     isValidTokenId[0] = true;
     isValidTokenId[1] = true;
@@ -36,5 +44,31 @@ contract SomeWordsClub is ERC1155 {
     return string(abi.encodePacked(baseURI, tokenId.toString(), ".json"));
   }
 
+  // has to mint the right NFTs
+  // cant mint more than the MAX_SUPPLY of the NFTs
+  // cant mint more than the MINT_LIMIT
+  // has to send the right amount of money
+  function mintYourWord(uint256 tokenId, uint256 numTokens) external payable nonReentrant {
+    require(isValidTokenId[tokenId] == true, "ERROR, out of bounds tokenId");
+    require(numTokens <= MINT_LIMIT, "ERROR, minting too many at a time");
+    require((totalSupply(tokenId) + numTokens) <= MAX_SUPPLY, "ERROR, minting would go over supply limit");
+    require(msg.value >= (numTokens * MINT_PRICE), "ERROR, not enough eth to buy");
+
+    _mint(msg.sender, tokenId, numTokens, "");
+  }
+
+  function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+    internal
+    override(ERC1155, ERC1155Supply)
+  {
+    super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+  }
+
+  function withdrawBalance() external onlyOwner {
+		uint256 _balance = address(this).balance;
+		require(_balance > 0, "No amount to withdraw");
+    console2.log(_balance, "balance in withdraw");
+		Address.sendValue(payable(owner()),_balance);
+  }
 
 }
--- blockchain/test/Contract.t.sol
+++ blockchain/test/Contract.t.sol
@@ -8,16 +8,85 @@ import "src/SomeWordsClub.sol";
 contract ContractTest is Test {
   SomeWordsClub public someWordsClub;
   string public defaultUri = "ipfs://QmcGda9o8iy5tB197fG8tVF4HzWPrbxHY4psu17gog2jX4/";
+  
+  address constant deployerAddress = 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84;
+  address constant devAddress = 0xfD7f2bfD60a332Aedd002b2CCf1D1a213259CC95;
+  address constant alice = 0x802B68893C0C316476a68554058E20C3601de3E4;
 
   function setUp() public {
+    vm.label(alice, "Alice");
+    hoax(alice, 1 ether);
     someWordsClub = new SomeWordsClub(defaultUri);
   }
 
   function testUri() public {
     string memory foundUri = someWordsClub.uri(0);
-    console2.log(foundUri, "<== foundUri");
-
     assertTrue(keccak256(bytes(foundUri)) == keccak256(bytes("ipfs://QmcGda9o8iy5tB197fG8tVF4HzWPrbxHY4psu17gog2jX4/0.json")));
+  }
+
+  function testMintSomeWords() public {
+    startHoax(devAddress, 1 ether);
+    console2.log(devAddress.balance, "balance of hoax dev");
+
+    someWordsClub.mintYourWord{value: 0.0125 ether}(0, 5);
+
+    uint256 devNFTBalance = someWordsClub.balanceOf(devAddress, 0);
+
+    assertTrue(devNFTBalance == 5);
+    assertTrue(address(someWordsClub).balance == 0.0125 ether);
+  }
+
+  function testCanMintAllTheNFTs() public {
+    startHoax(devAddress, 100 ether);
+
+    for (uint i = 0; i < 500; i++) {
+      someWordsClub.mintYourWord{value: 0.0125 ether}(0, 5);
+      someWordsClub.mintYourWord{value: 0.0125 ether}(1, 5);
+      someWordsClub.mintYourWord{value: 0.0125 ether}(2, 5);
+      someWordsClub.mintYourWord{value: 0.0125 ether}(3, 5);
+      someWordsClub.mintYourWord{value: 0.0125 ether}(4, 5);
+    }
+
+    assertTrue(someWordsClub.balanceOf(devAddress, 0) == 2500);
+    assertTrue(someWordsClub.balanceOf(devAddress, 1) == 2500);
+    assertTrue(someWordsClub.balanceOf(devAddress, 2) == 2500);
+    assertTrue(someWordsClub.balanceOf(devAddress, 3) == 2500);
+    assertTrue(someWordsClub.balanceOf(devAddress, 4) == 2500);
+  }
+
+  function testCanWithdrawBalance() public {
+    hoax(devAddress, 1 ether);
+    someWordsClub.mintYourWord{value: 0.0125 ether}(0, 5);
+
+    assertTrue(address(someWordsClub).balance == 0.0125 ether);
+
+    console2.log(someWordsClub.owner(), "owner");
+
+    vm.prank(alice);
+    someWordsClub.withdrawBalance();
+  }
+
+  function testFailCannotWithdrawAsNonOwner() public {
+    hoax(devAddress, 1 ether);
+    someWordsClub.mintYourWord{value: 0.0125 ether}(0, 5);
+
+    someWordsClub.withdrawBalance();
+  }
+
+  function testFailMintSingleWordOverLimit () public {
+    startHoax(devAddress, 10 ether);
+
+    for (uint i = 0; i <= 500; i++) {
+      someWordsClub.mintYourWord{value: 0.0125 ether}(0, 5);
+    }
+  }
+
+  function testFailMintOutOfBoundsId() public {
+    someWordsClub.mintYourWord(10, 5);
+  }
+
+  function testFailMintTooMany() public {
+    someWordsClub.mintYourWord(0, 10);
   }
 
   function testFailUriOutOfBounds() public view {
