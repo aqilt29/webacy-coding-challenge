6dfb3dfc8b88aa59979dc83afca5684c486f9a61
Aqil Thanawala <aqil.thanawala@gmail.com>
2022-06-30 15:11:42 +0100

    deployed contract to local anvil instance and added local deploy script to package.json

--- blockchain/script/Contract.s.sol
+++ blockchain/script/Contract.s.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "forge-std/console2.sol";
+import "../src/SomeWordsClub.sol";
 
-contract ContractScript is Script {
+contract Deploy is Script {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+        SomeWordsClub someWordsClub = new SomeWordsClub("ipfs://QmcGda9o8iy5tB197fG8tVF4HzWPrbxHY4psu17gog2jX4/");
     }
 }
--- blockchain/src/SomeWordsClub.sol
+++ blockchain/src/SomeWordsClub.sol
@@ -30,13 +30,17 @@ contract SomeWordsClub is ERC1155, ERC1155Supply, ReentrancyGuard, Ownable {
   mapping(uint256 => bool) public isValidTokenId;
 
 
-  constructor(string memory _tokenUri) ERC1155(_tokenUri) {
+  constructor(string memory _tokenUri) ERC1155("") {
     baseURI = _tokenUri;
     isValidTokenId[0] = true;
     isValidTokenId[1] = true;
     isValidTokenId[2] = true;
     isValidTokenId[3] = true;
     isValidTokenId[4] = true;
+  }
+
+  function updateUri(string memory _newUri) external onlyOwner {
+    baseURI = _newUri;
   }
 
   function uri(uint256 tokenId) public view virtual override returns (string memory) {
